5194d965ef5c0ed87699e08bbe76fc1dc4a94c3d
Nyarmith <powah.serge@gmail.com>
2016-12-12 20:20:20 -0500

    Added resolution info, put it in about handler for migration

--- src/com/fxtoy/view/StartMenuController.java
+++ src/com/fxtoy/view/StartMenuController.java
@@ -3,7 +3,11 @@ package com.fxtoy.view;
 import com.fxtoy.application.MainApplication;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Screen;
 
 public class StartMenuController {
 	
@@ -28,7 +32,16 @@ public class StartMenuController {
 	
 	@FXML
 	private void handleAbout() {
-		// TODO: display info alert box with credits
+		//TODO: Add credits
+		//Magical method to get screen resolution
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        String MaxResStr = "Max Resolution : " + primScreenBounds.getMaxX() + " x " + primScreenBounds.getMaxY() + "\n";
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Woah(!)");
+        alert.setHeaderText("\"Ab-OUT\"");
+        alert.setContentText(MaxResStr + "Help Me I'm In Virtual Space");
+
+        alert.showAndWait();
 	}
 	
 	@FXML
